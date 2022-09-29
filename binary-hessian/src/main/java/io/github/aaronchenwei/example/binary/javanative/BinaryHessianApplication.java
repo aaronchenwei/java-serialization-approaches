8154855dd2f0746ab97c630e45fd6f2687ba79f7
aaronchenwei <aaronchenwei@hotmail.com>
2022-09-29 19:42:12 +0800

    create project binary-hessian

---
+++ binary-hessian/src/main/java/io/github/aaronchenwei/example/binary/javanative/BinaryHessianApplication.java
@@ -0,0 +1,56 @@
+package io.github.aaronchenwei.example.binary.javanative;
+
+import com.caucho.hessian.io.Hessian2Output;
+import io.github.aaronchenwei.example.UserUtility;
+import io.github.aaronchenwei.example.entity.User;
+import java.io.File;
+import java.io.FileOutputStream;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author aaronchenwei
+ */
+@Slf4j
+public class BinaryHessianApplication {
+
+  private static final String FILE_PATH = "hessian-user.bin";
+
+  public static void main(String[] args) {
+    log.atInfo().log("Start to run...");
+    new BinaryHessianApplication().execute();
+  }
+
+  @SneakyThrows
+  private void execute() {
+    User user = UserUtility.createUser();
+
+    /*
+      Hessian doesn't support JSR-310 Date Types (such as LocalDateTime or etc.)
+     */
+    user.setTimestamp(null);
+
+    /*
+      Write out Java Object to file
+     */
+    try (var fileOutputStream = new FileOutputStream(FILE_PATH)) {
+      Hessian2Output hessian2Output = new Hessian2Output(fileOutputStream);
+      hessian2Output.startMessage();
+      hessian2Output.writeObject(user);
+      hessian2Output.completeMessage();
+      hessian2Output.close();
+    }
+
+    /*
+      Serialized Java Object size
+     */
+    File file = new File(FILE_PATH);
+    log.atInfo().log("File size = {}", file.length());
+
+    boolean ret = file.delete();
+    if (ret) {
+      log.atInfo().log("Serialized file has been deleted.");
+    }
+  }
+
+}
---
+++ binary-hessian/src/test/java/io/github/aaronchenwei/example/binary/javanative/BinaryHessianApplicationTests.java
@@ -0,0 +1,11 @@
+package io.github.aaronchenwei.example.binary.javanative;
+
+import org.junit.jupiter.api.Test;
+
+public class BinaryHessianApplicationTests {
+
+  @Test
+  public void test() {
+
+  }
+}
