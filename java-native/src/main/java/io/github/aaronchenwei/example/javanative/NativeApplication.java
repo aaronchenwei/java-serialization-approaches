631b58863dbd9ea0a18903eda6b6eb6534a29b81
aaronchenwei <aaronchenwei@hotmail.com>
2022-09-29 10:49:36 +0800

    Create project java-native

---
+++ java-native/src/main/java/io/github/aaronchenwei/example/javanative/NativeApplication.java
@@ -0,0 +1,15 @@
+package io.github.aaronchenwei.example.javanative;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author aaronchenwei
+ */
+@Slf4j
+public class NativeApplication {
+
+  public static void main(String[] args) {
+    log.atInfo().log("Start to run...");
+  }
+
+}
