5c03bb309cadbf4a0533502a578cc2fde15b49ef
aaronchenwei <aaronchenwei@hotmail.com>
2022-09-29 20:08:07 +0800

    Create project binary-hessian-lite

---
+++ binary-hessian-lite/src/test/java/io/github/aaronchenwei/example/binary/hessianlite/BinaryHessianLiteApplicationTests.java
@@ -0,0 +1,11 @@
+package io.github.aaronchenwei.example.binary.hessianlite;
+
+import org.junit.jupiter.api.Test;
+
+public class BinaryHessianLiteApplicationTests {
+
+  @Test
+  public void test() {
+
+  }
+}
