21ff25d109cbff842fc045001a3020b9ee711e6a
aaronchenwei <aaronchenwei@hotmail.com>
2022-09-29 11:22:16 +0800

    Create UserUtility.java

---
+++ common/src/main/java/io/github/aaronchenwei/example/UserUtility.java
@@ -0,0 +1,52 @@
+package io.github.aaronchenwei.example;
+
+import io.github.aaronchenwei.example.entity.User;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import lombok.experimental.UtilityClass;
+
+/**
+ * @author aaronchenwei
+ */
+@UtilityClass
+public class UserUtility {
+
+  public static User createUser() {
+    User user = new User();
+    user.setId(1L);
+    user.setNick("johnsmith");
+    user.setEmail("johnsmith@test.org");
+    user.setPhone("13888886666");
+    user.setBalance(10000.0);
+    LocalDate localDate = LocalDate.of(2000, 1, 1);
+    user.setBornAt(Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant()));
+    user.setFlag(0L);
+    user.setVip(false);
+    user.setTimestamp(LocalDateTime.now());
+    return user;
+  }
+
+  public static List<User> createUsers(int number) {
+    var users = new ArrayList<User>(number);
+    for (int i = 1; i <= number; i++) {
+      User user = new User();
+      user.setId(i);
+      user.setNick("johnsmith" + i);
+      user.setEmail("johnsmith" + i + "@test.org");
+      user.setPhone(Long.toString(13888880000L + i));
+      user.setBalance(10000.0 + i);
+      LocalDate localDate = LocalDate.of(2000, 1, 1);
+      user.setBornAt(Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant()));
+      user.setFlag(0L);
+      user.setVip(false);
+      user.setTimestamp(LocalDateTime.now());
+      users.add(user);
+    }
+    return users;
+  }
+
+}
