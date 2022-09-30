b2c22fb532e9dd919e640910c484d8a19d129f3a
aaronchenwei <aaronchenwei@hotmail.com>
2022-09-30 17:39:20 +0800

    test serialized object size with message pack

--- binary-msgpack-jackson/src/test/java/io/github/aaronchenwei/example/binary/msgpack/BinaryMessagePackApplicationTests.java
+++ binary-msgpack-jackson/src/test/java/io/github/aaronchenwei/example/binary/msgpack/BinaryMessagePackApplicationTests.java
@@ -1,12 +1,52 @@
 package io.github.aaronchenwei.example.binary.msgpack;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.aaronchenwei.example.PrimitiveDummyUtility;
+import io.github.aaronchenwei.example.entity.PrimitiveDummy;
+import java.io.ByteArrayOutputStream;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.msgpack.jackson.dataformat.MessagePackMapper;
 
+@Slf4j
 public class BinaryMessagePackApplicationTests {
 
-  @Test
-  public void test() {
+  private static final int MAX_ARRAY_SIZE = 1 << 20;
 
+  private static final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(MAX_ARRAY_SIZE);
+
+  private static final ObjectMapper messagePackMapper = new MessagePackMapper();
+
+  @BeforeAll
+  public static void beforeAll() {
+
+  }
+
+  @BeforeEach
+  public void beforeEach() {
+    byteArrayOutputStream.reset();
+  }
+
+  @Test
+  public void testMax() {
+    PrimitiveDummy maxDummy = PrimitiveDummyUtility.createMaxDummy();
+    execute(maxDummy);
+  }
+
+  @Test
+  public void testMin() {
+    PrimitiveDummy minDummy = PrimitiveDummyUtility.createMinDummy();
+    execute(minDummy);
+  }
+
+  @SneakyThrows
+  private void execute(PrimitiveDummy dummy) {
+    byteArrayOutputStream.write(messagePackMapper.writeValueAsBytes(dummy));
+
+    log.atInfo().log("Serialized Object size = {}", byteArrayOutputStream.size());
   }
 
 }
