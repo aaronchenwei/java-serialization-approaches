5555e02cfd6179a841e917d2bc41e2ce17f69abc
aaronchenwei <aaronchenwei@hotmail.com>
2022-09-30 17:10:01 +0800

    test serialized object size with JDK built-in

--- binary-java-native/src/test/java/io/github/aaronchenwei/example/binary/javanative/BinaryJavaNativeApplicationTests.java
+++ binary-java-native/src/test/java/io/github/aaronchenwei/example/binary/javanative/BinaryJavaNativeApplicationTests.java
@@ -1,12 +1,51 @@
 package io.github.aaronchenwei.example.binary.javanative;
 
+import io.github.aaronchenwei.example.PrimitiveDummyUtility;
+import io.github.aaronchenwei.example.entity.PrimitiveDummy;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 public class BinaryJavaNativeApplicationTests {
 
-  @Test
-  public void test() {
+  private static final int MAX_ARRAY_SIZE = 1 << 20;
 
+  private static final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(MAX_ARRAY_SIZE);
+
+  @BeforeAll
+  public static void beforeAll() {
+
+  }
+
+  @BeforeEach
+  public void beforeEach() {
+    byteArrayOutputStream.reset();
+  }
+
+  @Test
+  public void testMax() {
+    PrimitiveDummy maxDummy = PrimitiveDummyUtility.createMaxDummy();
+    execute(maxDummy);
+  }
+
+  @Test
+  public void testMin() {
+    PrimitiveDummy minDummy = PrimitiveDummyUtility.createMinDummy();
+    execute(minDummy);
+  }
+
+  @SneakyThrows
+  private void execute(PrimitiveDummy dummy) {
+    try (var objectOutputStream = new ObjectOutputStream(byteArrayOutputStream)) {
+      objectOutputStream.writeObject(dummy);
+    }
+
+    log.atInfo().log("Serialized Object size = {}", byteArrayOutputStream.size());
   }
 
 }
