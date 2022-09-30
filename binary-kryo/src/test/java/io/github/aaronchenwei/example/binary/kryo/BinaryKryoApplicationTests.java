3a2393484bb49b7269116b2e6505b4f80575afe2
aaronchenwei <aaronchenwei@hotmail.com>
2022-09-30 16:55:41 +0800

    test serialized object size with kryo

--- binary-kryo/src/test/java/io/github/aaronchenwei/example/binary/kryo/BinaryKryoApplicationTests.java
+++ binary-kryo/src/test/java/io/github/aaronchenwei/example/binary/kryo/BinaryKryoApplicationTests.java
@@ -1,12 +1,54 @@
 package io.github.aaronchenwei.example.binary.kryo;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Output;
+import io.github.aaronchenwei.example.PrimitiveDummyUtility;
+import io.github.aaronchenwei.example.entity.PrimitiveDummy;
+import java.io.ByteArrayOutputStream;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 public class BinaryKryoApplicationTests {
 
-  @Test
-  public void test() {
+  private static final int MAX_ARRAY_SIZE = 1 << 20;
 
+  private static final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(MAX_ARRAY_SIZE);
+
+  private static final Kryo kryo = new Kryo();
+
+  @BeforeAll
+  public static void beforeAll() {
+    kryo.setRegistrationRequired(false);
+  }
+
+  @BeforeEach
+  public void beforeEach() {
+    byteArrayOutputStream.reset();
+  }
+
+  @Test
+  public void testMax() {
+    PrimitiveDummy maxDummy = PrimitiveDummyUtility.createMaxDummy();
+    execute(maxDummy);
+  }
+
+  @Test
+  public void testMin() {
+    PrimitiveDummy minDummy = PrimitiveDummyUtility.createMinDummy();
+    execute(minDummy);
+  }
+
+  @SneakyThrows
+  private void execute(PrimitiveDummy dummy) {
+    try (var output = new Output(byteArrayOutputStream)) {
+      kryo.writeObject(output, dummy);
+    }
+
+    log.atInfo().log("Serialized Object size = {}", byteArrayOutputStream.size());
   }
 
 }
